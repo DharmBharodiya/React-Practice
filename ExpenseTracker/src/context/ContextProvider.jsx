8f085a266413497bb28742659ff61486b2cb00c1
DharmBharodiya <dharmbharodiya@gmail.com>
2025-06-16 23:58:05 +0530

    use of context api and useReducer in expense tracker app

--- ExpenseTracker/src/context/ContextProvider.jsx
+++ ExpenseTracker/src/context/ContextProvider.jsx
@@ -1,3 +1,56 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useReducer } from "react";
+
+const ExpenseContext = createContext();
+
+const initialState = {
+  transactions: [],
+};
+
+const expenseReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export const ExpenseProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(expenseReducer, initialState);
+
+  const addTransaction = (transaction) => {
+    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+  };
+
+  const deleteTransaction = (id) => {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  };
+
+  return (
+    <ExpenseContext.Provider
+      value={{
+        transactions: state.transactions,
+        addTransaction,
+        deleteTransaction,
+      }}
+    >
+      {children}
+    </ExpenseContext.Provider>
+  );
+};
+
+export const useExpenseContext = () => {
+  return useContext(ExpenseContext);
+};
 
 // context for transaction app will reside over here
